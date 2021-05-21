b68aa028f44d1444ff3766e3ad50f4d0517e06eb
Francisco Batista <fransta8@gmail.com>
2021-05-21 13:41:06 -0300

    Ajuste no display! Evintando o overflow.

--- src/script.js
+++ src/script.js
@@ -7,24 +7,24 @@ const btClear = document.querySelector('#clear');
 const btBack = document.querySelector('#back');
 
 // Apaga valores do display
-btClear.addEventListener('click', () => display.innerHTML = '');
+btClear.addEventListener('click', () => display.value = '');
 
 // Adiciona numeros no display
-if (display.innerHTML.length < 10) {
+if (display.value.length < 10) {
 	for (let i = 0; i < btNum.length; i++) {
 		btNum[i].addEventListener('click', function() {
-			display.innerHTML += btNum[i].innerText;
+			display.value += btNum[i].innerText;
 		})
 	}
 }
 
 // Desfaz últimos dígitos do display
 btBack.addEventListener('click', function() {
-	let arr = display.innerText.split('');
+	let arr = display.value.split('');
 	arr.pop();
 
 	let convert = arr.join('');
-	display.innerHTML = convert;
+	display.value = convert;
 	convert = '';
 })
 
